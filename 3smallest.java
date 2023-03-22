874a43ef2883127c0334c898bf2fa882c3faa6e4
shrutik Nandeshwar <123307004+Shrutik1711@users.noreply.github.com>
2023-03-22 19:54:08 +0530

    Update 3smallest.java

--- 3smallest.java
+++ 3smallest.java
@@ -4,13 +4,13 @@ public class SmallestNumber {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        // Accept three numbers from the user
+        
         System.out.print("Enter three numbers: ");
         int num1 = input.nextInt();
         int num2 = input.nextInt();
         int num3 = input.nextInt();
         
-        // Find the smallest number among the three
+        
         int smallest = num1;
         if (num2 < smallest) {
             smallest = num2;
@@ -19,7 +19,7 @@ public class SmallestNumber {
             smallest = num3;
         }
         
-        // Display the smallest number
+        
         System.out.println("The smallest number is " + smallest);
     }
 }
